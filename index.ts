2bfb5be7ae091fdef65af5a095f1e45afedfc4fe
nilsmanc <etereaiberia@gmail.com>
2023-05-02 07:04:48 +0300

    deploy

--- index.ts
+++ index.ts
@@ -2,13 +2,12 @@ import express, { Application } from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-import { MONGODB } from './variables'
 import { CinemaController, MovieController } from './controllers'
 
 const app: Application = express()
 
 mongoose
-  .connect(MONGODB as string)
+  .connect(process.env.MONGODB_URL as string)
   .then(() => console.log('Database OK'))
   .catch((err) => console.log('Database error', err))
 
@@ -22,6 +21,6 @@ app.get('/movies/cinema/:id', MovieController.getCinemaMovies)
 app.get('/cinemas', CinemaController.getAll)
 app.get('/cinemas/:id', CinemaController.getOne)
 
-app.listen(4444, () => {
+app.listen(process.env.PORT || 4444, () => {
   console.log('Server OK')
 })
